a698e001abef0c9c00a563f98218df3545a4d73f
luiqor <dianavashch@gmail.com>
2024-10-18 18:27:35 +0300

    feat: add run command

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,12 +1,13 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 
 using VersionCommand = Lab4.Commands.VersionCommand;
+using RunCommand = Lab4.Commands.RunCommand;
 using SetPathCommand = Lab4.Commands.SetPathCommand;
 
 namespace Lab4;
 
 [Command(Name = "labtool", Description = "Tool for running lab assignments")]
-[Subcommand(typeof(VersionCommand), typeof(SetPathCommand))]
+[Subcommand(typeof(VersionCommand), typeof(SetPathCommand), typeof(RunCommand))]
 class Program
 {
     public static int Main(string[] args)
---
+++ Lab4/Lab4/commands/RunCommand.cs
@@ -0,0 +1,75 @@
+using McMaster.Extensions.CommandLineUtils;
+using DotNetEnv;
+
+namespace Lab4.Commands;
+using Lab1 = LabLibrary.Lab1;
+using Lab2 = LabLibrary.Lab2;
+using Lab3 = LabLibrary.Lab3;
+
+[Command(Name = "run", Description = "Run lab tasks")]
+class RunCommand
+{
+    [Option(Description = "Specify input file", ShortName = "I", LongName = "input")]
+    public required string InputFile { get; set; }
+
+    [Option(Description = "Specify output file", ShortName = "o", LongName = "output")]
+    public required string OutputFile { get; set; }
+
+    [Argument(0, Description = "Lab name (lab1, lab2, lab3)")]
+    public required string Lab { get; set; }
+
+    private void OnExecute(CommandLineApplication app, IConsole console)
+    {
+        Env.Load(".env");
+
+        string? envLabPath = Environment.GetEnvironmentVariable("LAB_PATH");
+        Console.WriteLine($"Running {Lab} with input: {InputFile}, output: {OutputFile}, LAB_PATH: {envLabPath}");
+
+        if (string.IsNullOrEmpty(Lab))
+        {
+            console.WriteLine("Please specify a lab assignment.");
+            app.ShowHelp();
+            return;
+        }
+
+        string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        string inputPath = InputFile ?? Path.Combine(envLabPath ?? homeDirectory, "input.txt");
+        string outputPath = OutputFile ?? Path.Combine(envLabPath ?? homeDirectory, "output.txt");
+
+
+        Console.WriteLine($"Input path: {inputPath}");
+        Console.WriteLine($"Output path: {outputPath}");
+
+        if (!File.Exists(inputPath))
+        {
+            console.WriteLine($"Error: Input file not found at path '{inputPath}'");
+            return;
+        }
+
+        switch (Lab.ToLower())
+        {
+            case "lab1":
+                Lab1 lab1 = new();
+                lab1.Build();
+                lab1.Test();
+                lab1.Run(inputPath, outputPath);
+                break;
+            case "lab2":
+                Lab2 lab2 = new();
+                lab2.Build();
+                lab2.Test();
+                lab2.Run(inputPath, outputPath);
+                break;
+            case "lab3":
+                Lab3 lab3 = new();
+                lab3.Build();
+                lab3.Test();
+                lab3.Run(inputPath, outputPath);
+                break;
+            default:
+                console.WriteLine("Please specify a valid lab assignment.");
+                break;
+        }
+    }
+}
