88a85b4c4501902601bea968379e24d1fdbfa833
luiqor <dianavashch@gmail.com>
2024-10-18 18:27:21 +0300

    feat: add set-path command

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,11 +1,12 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 
 using VersionCommand = Lab4.Commands.VersionCommand;
+using SetPathCommand = Lab4.Commands.SetPathCommand;
 
 namespace Lab4;
 
 [Command(Name = "labtool", Description = "Tool for running lab assignments")]
-[Subcommand(typeof(VersionCommand))]
+[Subcommand(typeof(VersionCommand), typeof(SetPathCommand))]
 class Program
 {
     public static int Main(string[] args)
