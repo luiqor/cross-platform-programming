4013dbeae17cd7598b91dcb25cb2f0a61a470df6
luiqor <dianavashch@gmail.com>
2024-10-18 18:19:30 +0300

    feat: add initial lab4 projects

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,16 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+namespace Lab4;
+
+[Command(Name = "labtool", Description = "Tool for running lab assignments")]
+class Program
+{
+    public static int Main(string[] args)
+    {
+        return CommandLineApplication.Execute<Program>(args);
+    }
+
+    private void OnExecute(CommandLineApplication app)
+    {
+        app.ShowHelp();
+    }
+}
