078c6ade0eaca5751c4a695c1897058cd9c4e1dd
luiqor <dianavashch@gmail.com>
2024-10-18 18:25:06 +0300

    feat: implement lab library

---
+++ Lab4/LabLibrary/Lab1.cs
@@ -0,0 +1,6 @@
+﻿namespace LabLibrary;
+
+public class Lab1 : LabBase
+{
+    protected override string LabName => "Lab1";
+}
---
+++ Lab4/LabLibrary/Lab2.cs
@@ -0,0 +1,6 @@
+namespace LabLibrary;
+
+public class Lab2 : LabBase
+{
+    protected override string LabName => "Lab2";
+}
