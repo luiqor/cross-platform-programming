8108386dc4afc9e1abe9a33fd6fb7b4d80a1fc50
luiqor <dianavashch@gmail.com>
2024-11-19 22:16:04 +0200

    feat: add home page

--- Lab13/Lab13-Client/src/App.jsx
+++ Lab13/Lab13-Client/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { AppRoute } from "./constants/constants";
+import { Home } from "./pages/pages";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      element: <Home />,
+      path: AppRoute.ROOT,
+    },
+    {
+      element: <Navigate to={AppRoute.ROOT} replace />,
+      path: AppRoute.ANY,
+    },
+  ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
---
+++ Lab13/Lab13-Client/src/pages/home/Home.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => {
+  const [message, setMessage] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch('/api/home')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <Container>
+      <section>
+        <h3>Підключення до серверу?</h3>
+        {message ? <p>{message}</p> : <p>Loading...</p>}
+      </section>
+      <section className="text-center">
+        <h1>Лабораторна №5</h1>
+      </section>
+      <section>
+        <h2>Веб застосунок (ASP.NET Core MVC) складається з двох проектів:</h2>
+        <p>a. Безпосередньо веб додаток</p>
+        <p>b. Бібліотека класів, що дає змогу запускати практичні 1, 2 або 3</p>
+      </section>
+      <section>
+        <h3>Сторінка логіну, реєстрації та профілю користувача:</h3>
+        <Container className="mt-3 mb-4">
+          <Row>
+            <Col md={4}>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Логін</Card.Title>
+                  <Card.Text>Увійдіть до свого облікового запису.</Card.Text>
+                  <Button onClick={() => { navigate('/login')}} variant="primary">Увійти</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Реєстрація</Card.Title>
+                  <Card.Text>Створіть новий обліковий запис.</Card.Text>
+                  <Button onClick={() => { navigate('/register')}} variant="primary">Зареєструватися</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Профіль користувача</Card.Title>
+                  <Card.Text>Перегляньте свій профіль.</Card.Text>
+                  <Button onClick={() => { navigate('/profile')}} variant="primary">Профіль</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <h3>Сторінки практичних завдань:</h3>
+        <Container className="mt-3">
+          <Row>
+            <Col md={4}>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Лаб №1</Card.Title>
+                  <Card.Text>Запустіть практичну 1 та отримайте результат.</Card.Text>
+                  <Button onClick={() => { navigate('/lab1')}} variant="primary">Го</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Лаб №2</Card.Title>
+                  <Card.Text>Запустіть практичну 2 та отримайте результат.</Card.Text>
+                  <Button onClick={() => { navigate('/lab2')}} variant="primary">Го</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="text-center">
+                <Card.Body>
+                  <Card.Title>Лаб №3</Card.Title>
+                  <Card.Text>Запустіть практичну 3 та отримайте результат.</Card.Text>
+                  <Button onClick={() => { navigate('/lab3')}} variant="primary">Го</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Container>
+  );
+}
+
+export { Home };
