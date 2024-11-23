b31d44ad8a0a1c183471b95a8e9c32c2b6df604d
luiqor <dianavashch@gmail.com>
2024-11-24 00:04:52 +0200

    add frontend login

--- Lab13/Lab13-Client/src/pages/login/login.jsx
+++ Lab13/Lab13-Client/src/pages/login/login.jsx
@@ -1,31 +1,63 @@
+import { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      await fetch('/api/account/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        credentials: 'include',
+      });
+
+    } catch (error) {
+      setMessage(result.message ?? 'Error submitting data');
+    }
+  };
+
   return (
     <Container className="mt-5">
       <h2>Login</h2>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-danger">
-            {/* Validation message for Email */}
-          </Form.Text>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-          <Form.Text className="text-danger">
-            {/* Validation message for Password */}
-          </Form.Text>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           Login
         </Button>
         <div className="text-danger mb-3">
-          {/* Validation summary */}
+          {message}
         </div>
       </Form>
     </Container>
