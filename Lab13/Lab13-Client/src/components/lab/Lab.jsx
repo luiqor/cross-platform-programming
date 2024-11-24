3cd2f0f95ad11fe95bbc03ea039aabad10d70aba
luiqor <dianavashch@gmail.com>
2024-11-24 16:06:27 +0200

    add lab field as required

--- Lab13/Lab13-Client/src/components/lab/Lab.jsx
+++ Lab13/Lab13-Client/src/components/lab/Lab.jsx
@@ -1,6 +1,21 @@
+import React, { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
 
 const Lab = ({ index, handleSubmit, errorMessage, children }) => {
+  const [inputData, setInputData] = useState("");
+  const [inputError, setInputError] = useState("");
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInputData(value);
+
+    if (!value) {
+      setInputError("This field is required");
+    } else {
+      setInputError("");
+    }
+  };
+
   return (
     <Container className="mt-5">
       <h2>Run Lab №{index}</h2>
@@ -8,8 +23,18 @@ const Lab = ({ index, handleSubmit, errorMessage, children }) => {
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formInputData">
           <Form.Label>Введіть вхідні дані для лаб</Form.Label>
-          <Form.Control as="textarea" rows={10} cols={50} />
-          <Form.Text className="text-danger"></Form.Text>
+          <Form.Control
+            as="textarea"
+            rows={10}
+            cols={50}
+            value={inputData}
+            onChange={handleChange}
+            isInvalid={!!inputError}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            {inputError}
+          </Form.Control.Feedback>
         </Form.Group>
         <Button variant="primary" type="submit">
           Ок
